3d355e132b2b30178674e12a36a68296a70505be
roma-pi <roma_pi@mail.ru>
2022-11-16 08:06:26 +0500

    Map Scale Fixed

--- js/map.js
+++ js/map.js
@@ -8,7 +8,7 @@ function init(){
         // Порядок по умолчанию: «широта, долгота».
         // Чтобы не определять координаты центра карты вручную,
         // воспользуйтесь инструментом Определение координат.
-        bounds: [[55.74512880425985, 37.60109379 + (37.59266459383299 - 37.60109379) / 1920 * document.body.clientWidth], [55.779052985859074, 37.60109379 + (37.69892298616699 - 37.60109379) / 1920 * document.body.clientWidth]],
+        center: [55.75851460, 37.60109379],
         // Уровень масштабирования. Допустимые значения:
         // от 0 (весь мир) до 19.
         zoom: 14,
@@ -35,34 +35,5 @@ function init(){
 
   // Размещение геообъекта на карте.
   myMap.geoObjects.add(myPlacemark);
-
-  if (document.body.clientWidth <= 500) {
-    myMap.controls.get('geolocationControl').options.set('position', {
-      top: '60%',
-      right: '10px'
-    });
-  } else {
-    myMap.controls.get('geolocationControl').options.set('position', {
-      top: '50%',
-      right: '10px'
-    });
-  }
-
-  window.addEventListener('resize', function() {
-    myMap.setBounds([[55.74512880425985, 37.60109379 + (37.59266459383299 - 37.60109379) / 1920 * document.body.clientWidth], [55.779052985859074, 37.60109379 + (37.69892298616699 - 37.60109379) / 1920 * document.body.clientWidth]]);
-
-    if (document.body.clientWidth <= 500) {
-      myMap.controls.get('geolocationControl').options.set('position', {
-        top: '60%',
-        right: '10px'
-      });
-    } else {
-      myMap.controls.get('geolocationControl').options.set('position', {
-        top: '50%',
-        right: '10px'
-      });
-    }
-  })
-
 }
 
